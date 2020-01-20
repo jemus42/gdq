b6e927225398d4820865da91924d27812a5bae62
Lukas Burk <lukas@quantenbrot.de>
2020-01-20 22:53:40 +0100

    Filename consistency

--- _build.R
+++ _build.R
@@ -2,12 +2,22 @@
 
 rmarkdown::render_site(".")
 
-if (!requireNamespace("slackr")) {
-  remotes::install_github("hrbrmstr/slackr")
-}
+# if (!requireNamespace("slackr")) {
+#   remotes::install_github("hrbrmstr/slackr")
+# }
+#
+# library(slackr)
+# slackr_setup(config_file = "/opt/tadaadata/.slackr")
+#
+# msg <- paste0(lubridate::now(tzone = "CET"), ": Built https://gdq.tadaa-data.de")
+# text_slackr(msg, channel = "#gdq", username = "tadaabot", preformatted = FALSE)
 
-library(slackr)
-slackr_setup(config_file = "/opt/tadaadata/.slackr")
 
-msg <- paste0(lubridate::now(tzone = "CET"), ": Built https://gdq.tadaa-data.de")
-text_slackr(msg, channel = "#gdq", username = "tadaabot", preformatted = FALSE)
+x <- processx::run(
+  command = "rsync",
+  args = c("-rltv", "_site/", "pearson:'/mnt/data/websites/gdq.tadaa-data.de/'"),
+  echo_cmd = TRUE,
+  echo = FALSE,
+  spinner = TRUE,
+  error_on_status = TRUE
+)
