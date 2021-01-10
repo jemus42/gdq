19e6fe5a16048bdbd22882471f265e0b753d927c
Lukas Burk <lukas@quantenbrot.de>
2021-01-10 16:42:13 +0100

    Add new event dates

--- 00_helpers-gdqtracker.R
+++ 00_helpers-gdqtracker.R
@@ -138,7 +138,7 @@ assemble_runs <- function(events = NULL) {
 # Proper event dates ----
 # https://en.wikipedia.org/wiki/Games_Done_Quick#List_of_marathons
 event_dates <- tribble(
-  ~event,     ~start,            ~end,
+  ~event,     ~start,                        ~end,
   "AGDQ2011", ymd("2011-01-06", tz = "UTC"), ymd("2011-01-11", tz = "UTC"),
   "AGDQ2012", ymd("2012-01-04", tz = "UTC"), ymd("2012-01-09", tz = "UTC"),
   "AGDQ2013", ymd("2013-01-06", tz = "UTC"), ymd("2013-01-12", tz = "UTC"),
@@ -149,6 +149,7 @@ event_dates <- tribble(
   "AGDQ2018", ymd("2018-01-07", tz = "UTC"), ymd("2018-01-14", tz = "UTC"),
   "AGDQ2019", ymd("2019-01-06", tz = "UTC"), ymd("2019-01-12", tz = "UTC"),
   "AGDQ2020", ymd("2020-01-05", tz = "UTC"), ymd("2020-01-12", tz = "UTC"),
+  "AGDQ2021", ymd("2021-01-03", tz = "UTC"), ymd("2021-01-10", tz = "UTC"),
   "SGDQ2011", ymd("2011-08-04", tz = "UTC"), ymd("2011-08-06", tz = "UTC"),
   "SGDQ2012", ymd("2012-05-24", tz = "UTC"), ymd("2012-05-28", tz = "UTC"),
   "SGDQ2013", ymd("2013-07-25", tz = "UTC"), ymd("2013-07-30", tz = "UTC"),
@@ -157,7 +158,9 @@ event_dates <- tribble(
   "SGDQ2016", ymd("2016-07-03", tz = "UTC"), ymd("2016-07-09", tz = "UTC"),
   "SGDQ2017", ymd("2017-07-02", tz = "UTC"), ymd("2017-07-09", tz = "UTC"),
   "SGDQ2018", ymd("2018-06-24", tz = "UTC"), ymd("2018-07-01", tz = "UTC"),
-  "SGDQ2019", ymd("2019-06-23", tz = "UTC"), ymd("2019-06-30", tz = "UTC")
+  "SGDQ2019", ymd("2019-06-23", tz = "UTC"), ymd("2019-06-30", tz = "UTC"),
+  "SGDQ2020", ymd("2020-08-16", tz = "UTC"), ymd("2020-08-23", tz = "UTC")
+
 ) %>%
   mutate(event_duration = start %--% end / ddays(1))
 
