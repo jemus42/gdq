0c2bc43ca3dd0be6bd98afd8b62274810d20bea9
Lukas Burk <lukas@quantenbrot.de>
2023-06-11 02:54:06 +0200

    Note currency API just in case

--- R/globals.R
+++ R/globals.R
@@ -2,6 +2,7 @@ gdq_base_url <- "https://gamesdonequick.com"
 
 # Guess I need to keep track of that number now.
 # Last checked 2023-06-06
+# jsonlite::read_json("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/eur.json")$eur
 .usd_to_eur <- 0.93
 
 
