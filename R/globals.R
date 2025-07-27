40eb94bd5d3bbe16dc73676bb4010ccf2a97fc25
Lukas Burk <lukas@quantenbrot.de>
2025-07-27 04:05:32 +0200

    Gobals

--- R/globals.R
+++ R/globals.R
@@ -6,5 +6,12 @@ gdq_base_url <- "https://tracker.gamesdonequick.com"
 
 # To be removed over time as code is made more... gooder.
 globalVariables(c(
-  "."
+  ".",
+  "end_runs",
+  "event_index",
+  "gdq_runs",
+  "run_duration",
+  "run_end",
+  "run_start",
+  "start_runs"
 ))
